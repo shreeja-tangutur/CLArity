d98f515d4f91f323025700bfe0a2e71b75bb4ca2
DJPark1213 <tfv4cm@virginia.edu>
2025-02-23 17:25:01 -0500

    postrgresql setup

--- claproject/mysite/settings.py
+++ claproject/mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import sys
 from pathlib import Path
 import os
 
@@ -74,12 +74,26 @@ WSGI_APPLICATION = 'claproject.mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+if 'test' in sys.argv:
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.postgresql',
+			'NAME': 'test_claproject',
+			'USER': 'b26_user',
+			'PASSWORD': 'b26password',
+			'HOST': 'localhost',
+			'PORT': '5432',
+		}
+	}
+else:
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.postgresql',
+			'NAME': 'claproject',
+			"OPTIONS": {"service": "my_service",},
+		}
 }
+
 
 
 # Password validation
