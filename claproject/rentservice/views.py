243e4453271e7fa1f13522c91c4ada1616535d97
Amelia Chen <amchen.0t7@gmail.com>
2025-02-23 11:33:03 -0500

    delete extra database, clean up views.py and sign_in.html

--- claproject/rentservice/views.py
+++ claproject/rentservice/views.py
@@ -13,8 +13,6 @@ from django.contrib.auth.models import User, Group
 
 @csrf_exempt
 def sign_in(request):
-    """Render the login page or redirect authenticated users to the correct dashboard."""
-    print(request.user.groups.all())  # Check which groups the user belongs to
     if request.user.is_authenticated:
         if request.user.groups.filter(name="Librarians").exists():
             return redirect('librarian_dashboard')
@@ -26,9 +24,6 @@ def sign_in(request):
 
 @csrf_exempt
 def auth_receiver(request):
-    """Handles Google login and assigns users to groups if needed."""
-    print("Inside auth_receiver")
-    
     token = request.POST.get('credential')
     if not token:
         return HttpResponse("No token provided", status=400)
@@ -43,11 +38,10 @@ def auth_receiver(request):
     email = user_data.get("email")
     name = user_data.get("name")
 
-    # Get or create user
     user, created = User.objects.get_or_create(username=email, defaults={"email": email, "first_name": name})
 
+    # user is patron by default
     if created:
-        # Assign the user to the "Patron" group by default
         patron_group, _ = Group.objects.get_or_create(name="Patron")
         user.groups.add(patron_group)
 
