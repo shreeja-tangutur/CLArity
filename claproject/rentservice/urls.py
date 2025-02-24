dc0ebd69c7e3e05f2564345564cbd942ea43e3e4
Shreeja Tangutur <ebz4sn@virginia.edu>
2025-02-24 16:48:44 -0500

    made changes for basic ui

--- claproject/rentservice/urls.py
+++ claproject/rentservice/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('auth-receiver', views.auth_receiver, name='auth_receiver'),
     path('librarian-dashboard/', views.librarian_dashboard, name='librarian_dashboard'),
     path('patron-dashboard/', views.patron_dashboard, name='patron_dashboard'),
+
 ]
