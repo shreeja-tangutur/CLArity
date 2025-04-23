9007e1a1c18e6260360e10d013c9dad6e533699b
Shreeja Tangutur <ebz4sn@virginia.edu>
2025-04-23 09:27:16 -0400

    more changes

--- claproject/rentservice/views.py
+++ claproject/rentservice/views.py
@@ -605,19 +605,19 @@ def view_borrow_requests(request):
             borrow_request.is_complete = False  # Still active
             borrow_request.item.mark_as_borrowed()
 
-            # Notification.objects.create(
-            #     user=borrow_request.user,
-            #     message=f"Your borrow request for '{borrow_request.item.title}' has been approved!"
-            # )
+            Notification.objects.create(
+                user=borrow_request.user,
+                message=f"Your borrow request for '{borrow_request.item.title}' has been approved!"
+            )
 
         elif action == "decline":
             borrow_request.status = "declined"
             borrow_request.is_complete = True  # Done
 
-            # Notification.objects.create(
-            #     user=borrow_request.user,
-            #     message=f"Your borrow request for '{borrow_request.item.title}' has been declined."
-            # )
+            Notification.objects.create(
+                user=borrow_request.user,
+                message=f"Your borrow request for '{borrow_request.item.title}' has been declined."
+            )
 
         borrow_request.save()
         return redirect("view_borrow_requests")
@@ -643,7 +643,7 @@ def respond_borrow_request(request, request_id, action):
         message = f"Your borrow request for '{borrow_request.item.title}' was declined!"
 
     borrow_request.save()
-    #Notification.objects.create(user=borrow_request.user, message=message)
+    Notification.objects.create(user=borrow_request.user, message=message)
     return redirect('view_borrow_requests')
 
 @login_required
